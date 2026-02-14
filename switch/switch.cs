2f4723d8058a3aea0f86472d465e4d81678ab765
Małgorzata Dąbrowska <malgorzata_dabrowska@hotmail.com>
2026-02-14 09:44:54 +0100

    switch

---
+++ switch/switch.cs
@@ -0,0 +1,30 @@
+﻿﻿using System;
+using System.Text;
+using System.Threading.Channels;
+
+namespace FirstProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            switch (DateTime.Now.DayOfWeek)
+            {
+                case DayOfWeek.Monday:
+                    Console.WriteLine("It's Monday ;(");
+                    break;
+                case DayOfWeek.Friday:
+                    Console.WriteLine("The last day of the work week.");
+                    break;
+                case DayOfWeek.Sunday:
+                case DayOfWeek.Saturday:
+                    Console.WriteLine("The weekend!");
+                    break;
+                default:
+                    Console.WriteLine("The middle of the work week.");
+                    break;
+
+            }
+        }
+    }
+}
